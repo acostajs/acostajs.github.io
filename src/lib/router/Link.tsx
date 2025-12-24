c9c172dbf16cdf62d2098bbe14f15e18e220d8dd
Juan Acosta <acosta.juan@icloud.com>
2025-12-24 10:00:08 -0500

    Setup Portfolio Project
    Setup Bun scripts, actions.yml for CI, Implemented router, routes, link, and hooks to use the project as an SPA, and restructured the project files.

---
+++ src/lib/router/Link.tsx
@@ -0,0 +1,24 @@
+import { useContext } from "react";
+import type { MouseEvent, ReactNode } from "react";
+import { NavigationContext } from "./context.ts";
+
+type LinkProps = {
+  to: string;
+  children: ReactNode;
+};
+
+/**
+ * When clicked, navigate `to` the given pathname.
+ */
+export function Link({ to, children }: LinkProps) {
+  const { setPathname: navigate } = useContext(NavigationContext);
+  function handleClick(e: MouseEvent): void {
+    e.preventDefault();
+    navigate(to);
+  }
+  return (
+    <a href={to} onClick={handleClick}>
+      {children}
+    </a>
+  );
+}
