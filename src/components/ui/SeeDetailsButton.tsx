8334021bd661692c1dd382a16d36b5a25be3fb38
Juan Acosta <acosta.juan@icloud.com>
2026-01-07 15:48:55 -0500

    Implemented Project Details Button
    still needs work but I have to push to see if it is fetching properly

--- src/components/ui/SeeDetailsButton.tsx
+++ src/components/ui/SeeDetailsButton.tsx
@@ -1,18 +1,9 @@
-// import type { Repository } from "@/types";
-// import type { ReactElement } from "react";
+import type { ReactElement } from "react";
 
-// type SeeDetailsButtonProps = {
-// 	repo: Repository;
-// };
-
-// export function SeeDetailsButton({
-// 	repo,
-// }: SeeDetailsButtonProps): ReactElement {
-// 	function handleClick() {}
-
-// 	return (
-// 		<button onClick={handleClick} className="see-details">
-// 			See Details...
-// 		</button>
-// 	);
-// }
+export function SeeDetailsButton(): ReactElement {
+  return (
+    <button popoverTarget="details" className="details-btn">
+      View Case Study
+    </button>
+  );
+}
