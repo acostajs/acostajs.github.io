916067ae72c7d0d04e6b797485486136acbdd8ac
Juan Acosta <acosta.juan@icloud.com>
2026-01-10 13:09:43 -0500

    Styled Details Page
    Responsiveness included, polishing will come later.

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -31,131 +31,175 @@ export function Details(): ReactElement {
   } = json;
 
   return (
-    <article>
-      <header>
+    <article className="details">
+      <header className="details-header flex flex-column gap-md">
         <h1>{meta.title}</h1>
 
-        <p>
-          {meta.type} · {meta.role}
-        </p>
+        <div className="details-header-bottom flex-between flex-wrap">
+          <div className="tag flex gap-md">
+            <span>{meta.type}</span>
+            <span>{meta.role}</span>
+          </div>
 
-        <p>Status: {meta.status}</p>
+          <p className="tag">
+            Status: <span className="details-status">{meta.status}</span>
+            {" "}
+          </p>
+        </div>
       </header>
 
-      <section>
-        <h2>Overview</h2>
-        <p>{overview.summary}</p>
+      <section className="details-overview">
+        <div className="details-section-title">
+          <h2>Overview</h2>
+        </div>
+        <div className="details-section-text">
+          <h3>Quick Summary</h3>
+          <p>{overview.summary}</p>
+          <h3>Problem</h3>
+          <p>{overview.problem}</p>
 
-        <h3>Problem</h3>
-        <p>{overview.problem}</p>
-
-        <h3>Solution</h3>
-        <p>{overview.solution}</p>
+          <h3>Solution</h3>
+          <p>{overview.solution}</p>
+        </div>
       </section>
 
-      {images.length > 0 && (
-        <section>
-          <h2>Project Preview</h2>
+      <div className="details-grid grid gap-xl">
+        <div className="left">
+          {images.length > 0 && (
+            <section className="details-carrousel">
+              <div className="details-section-title">
+                <h2>Project Preview</h2>
+              </div>
 
-          {images.map((src, index) => (
-            <figure key={src}>
-              <img src={src} alt={`${meta.title} screenshot ${index + 1}`} />
-            </figure>
-          ))}
-        </section>
-      )}
+              <div className="carousel-container">
+                <ul className="carousel">
+                  {images.map((src, index) => (
+                    <li>
+                      <figure key={src}>
+                        <img
+                          src={src}
+                          alt={`${meta.title} screenshot ${index + 1}`}
+                        />
+                      </figure>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </section>
+          )}
 
-      {responsibilities.length > 0 && (
-        <section>
-          <h2>Responsibilities</h2>
-          <ul>
-            {responsibilities.map((item) => <li key={item}>{item}</li>)}
-          </ul>
-        </section>
-      )}
+          {key_features.length > 0 && (
+            <section className="details-features">
+              <h2>Key Features</h2>
+              <ul className="details-list">
+                {key_features.map((feature) => <li key={feature}>{feature}</li>)}
+              </ul>
+            </section>
+          )}
 
-      {key_features.length > 0 && (
-        <section>
-          <h2>Key Features</h2>
-          <ul>
-            {key_features.map((feature) => <li key={feature}>{feature}</li>)}
-          </ul>
-        </section>
-      )}
+          {responsibilities.length > 0 && (
+            <section className="details-features">
+              <h2>Responsibilities</h2>
+              <ul className="details-list">
+                {responsibilities.map((item) => <li key={item}>{item}</li>)}
+              </ul>
+            </section>
+          )}
 
-      <section>
-        <h2>Architecture</h2>
+          {challenges_and_learnings.length > 0 && (
+            <section className="details-features">
+              <h2>Challenges & Learnings</h2>
+              <ul className="details-list">
+                {challenges_and_learnings.map((item) => <li key={item}>{item}</li>)}
+              </ul>
+            </section>
+          )}
 
-        <p>
-          <strong>Approach:</strong> {architecture.approach}
-        </p>
+          {future_improvements.length > 0 && (
+            <section className="details-features">
+              <h2>Future Improvements</h2>
+              <ul className="details-list">
+                {future_improvements.map((item) => <li key={item}>{item}</li>)}
+              </ul>
+            </section>
+          )}
+        </div>
 
-        <p>
-          <strong>Data flow:</strong> {architecture.data_flow}
-        </p>
+        <div className="right">
+          {tech_stack.length > 0 && (
+            <section className="details-stack">
+              <div className="details-section-title">
+                <h2>Tech Stack</h2>
+              </div>
+              <div className="details-section-text">
+                <ul className=" flex flex-wrap gap-md">
+                  {tech_stack.map((tech) => (
+                    <li key={tech} className="details-stack-item tag">
+                      {tech}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </section>
+          )}
 
-        <p>
-          <strong>Deployment:</strong> {architecture.deployment}
-        </p>
-      </section>
+          <section className="details-architecture">
+            <div className="details-section-title">
+              <h2>Architecture</h2>
+            </div>
 
-      {tech_stack.length > 0 && (
-        <section>
-          <h2>Tech Stack</h2>
-          <ul>
-            {tech_stack.map((tech) => <li key={tech}>{tech}</li>)}
-          </ul>
-        </section>
-      )}
+            <div className="details-section-text">
+              <ul className="details-list">
+                <li>
+                  <strong>Approach:</strong> {architecture.approach}
+                </li>
+                <li>
+                  <strong>Data flow:</strong> {architecture.data_flow}
+                </li>
+                <li>
+                  <strong>Deployment:</strong> {architecture.deployment}
+                </li>
+              </ul>
+            </div>
+          </section>
 
-      {challenges_and_learnings.length > 0 && (
-        <section>
-          <h2>Challenges & Learnings</h2>
-          <ul>
-            {challenges_and_learnings.map((item) => <li key={item}>{item}</li>)}
-          </ul>
-        </section>
-      )}
+          <section className="details-links">
+            <div className="details-section-title">
+              <h2>Links</h2>
+            </div>
+            <div className="details-section-text flex flex-column gap-md">
+              <a
+                className="tag details-link"
+                href={links.source_code}
+                target="_blank"
+              >
+                Source Code
+              </a>
 
-      {skills_demonstrated.length > 0 && (
-        <section>
-          <h2>Skills Demonstrated</h2>
-          <ul>
-            {skills_demonstrated.map((skill) => <li key={skill}>{skill}</li>)}
-          </ul>
-        </section>
-      )}
+              <a
+                className="tag details-link"
+                href={links.live_demo}
+                target="_blank"
+              >
+                Live Demo
+              </a>
+            </div>
+          </section>
 
-      {future_improvements.length > 0 && (
-        <section>
-          <h2>Future Improvements</h2>
-          <ul>
-            {future_improvements.map((item) => <li key={item}>{item}</li>)}
-          </ul>
-        </section>
-      )}
-
-      <footer>
-        <h2>Links</h2>
-
-        <ul>
-          <li>
-            <a href={links.live_demo} target="_blank" rel="noopener noreferrer">
-              Live Demo
-            </a>
-          </li>
-
-          <li>
-            <a
-              href={links.source_code}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Source Code
-            </a>
-          </li>
-        </ul>
-      </footer>
+          {skills_demonstrated.length > 0 && (
+            <section className="details-skills">
+              <div className="details-section-title">
+                <h2>Skills Demonstrated</h2>
+              </div>
+              <div className="details-section-text">
+                <ul className="details-list">
+                  {skills_demonstrated.map((skill) => <li key={skill}>{skill}</li>)}
+                </ul>
+              </div>
+            </section>
+          )}
+        </div>
+      </div>
     </article>
   );
 }
