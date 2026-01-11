764fa5213bdc64f2bacaf971818ffaa5d8a6ac85
Juan Acosta <acosta.juan@icloud.com>
2026-01-11 17:08:03 -0500

    Refactor Projects

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,17 +1,22 @@
 import { ContactForm } from "@/components/contact";
 import type { ReactElement } from "react";
 
+function ContactHeader(): ReactElement {
+  return (
+    <section className="section-title section-highlight">
+      <h1>Contact</h1>
+      <p>
+        If you’d like to connect, discuss an opportunity, or learn more about my work, feel free to reach out using the
+        form below or through the links provided.
+      </p>
+    </section>
+  );
+}
+
 export function Contact(): ReactElement {
   return (
     <article>
-      <section className="section-title section-highlight">
-        <h1>Contact</h1>
-        <p>
-          If you’d like to connect, discuss an opportunity, or learn more about my work, feel free to reach out using
-          the form below or through the links provided.
-        </p>
-      </section>
-
+      <ContactHeader />
       <ContactForm />
     </article>
   );
