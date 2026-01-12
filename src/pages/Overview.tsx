fa77f9ce88d0c1578c617009dcabfc0a356c9a10
Juan Acosta <acosta.juan@icloud.com>
2026-01-11 20:22:04 -0500

    Finish this branch with some styling changes
     Changed main background color, and type-font.

--- src/pages/Overview.tsx
+++ src/pages/Overview.tsx
@@ -1,12 +1,27 @@
 import { OverviewSection, ProjectSection } from "@/components/overview";
 import { useGitHub } from "@/context/GitHubContext";
 import type { ReactElement } from "react";
+import { BiSolidGame } from "react-icons/bi";
+
+function OverviewHeader(): ReactElement {
+  return (
+    <section className="overview-header flex flex-column gap-xl">
+      <div className="flex-left gap-md">
+        <BiSolidGame size={45} />
+        <p>It's good to see you!</p>
+      </div>
+
+      <p>I'm Juan, a Montreal-based Colombian-Canadian Junior web developer.</p>
+    </section>
+  );
+}
 
 export function Overview(): ReactElement {
   const { repos, readme } = useGitHub();
 
   return (
     <article>
+      <OverviewHeader />
       <OverviewSection readme={readme} />
       <ProjectSection repos={repos} />
     </article>
