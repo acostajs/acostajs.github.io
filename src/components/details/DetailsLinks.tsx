1650a329e5fa434c367b55b9ebfebf3b985767d2
Juan Acosta <acosta.juan@icloud.com>
2026-01-15 18:12:25 -0500

    implement conditions on lins in details

--- src/components/details/DetailsLinks.tsx
+++ src/components/details/DetailsLinks.tsx
@@ -1,4 +1,5 @@
 import type { ReactElement } from "react";
+import { ErrorMessage } from "../ui";
 
 type DetailsLinksProps = {
   source_code: string;
@@ -9,19 +10,29 @@ export function DetailsLinks({
   source_code,
   live_demo,
 }: DetailsLinksProps): ReactElement {
+  if (source_code.length === 0 && live_demo.length === 0) {
+    return (
+      <ErrorMessage error_message="Links coming soon. The project is currently running locally and will be deployed shortly." />
+    );
+  }
+
   return (
     <section className="details-links">
       <div className="details-section-title">
         <h2>Links</h2>
       </div>
       <div className="details-section-text flex flex-column gap-md">
-        <a className="tag details-link" href={source_code} target="_blank">
-          Source Code
-        </a>
+        {source_code.length > 0 && (
+          <a className="tag details-link" href={source_code} target="_blank">
+            Source Code
+          </a>
+        )}
 
-        <a className="tag details-link" href={live_demo} target="_blank">
-          Live Demo
-        </a>
+        {live_demo.length > 0 && (
+          <a className="tag details-link" href={live_demo} target="_blank">
+            Live Demo
+          </a>
+        )}
       </div>
     </section>
   );
